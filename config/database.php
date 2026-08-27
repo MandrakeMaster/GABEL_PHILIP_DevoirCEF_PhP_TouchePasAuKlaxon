f0d7965ffc51f70541459c39210666ac1b816ba4
MandrakeMaster <gabelphilip@hotmail.fr>
2026-08-27 10:06:53 +0200

    feat(db): création de la classe de connexion PDO sécurisée

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fichier de configuration de la base de données.
+ * 
+ * Retourne un tableau associatif contenant les paramètres de connexion PDO.
+ * 
+ * @author Philip Gabel
+ * @version 1.0
+ */
+
+return [
+    'host' => 'localhost',
+    'dbname' => 'touche_pas_au_klaxon',
+    'username' => 'root',
+    'password' => '', 
+    'charset' => 'utf8mb4'
+];
